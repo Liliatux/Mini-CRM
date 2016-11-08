3fd979531d93e146104aeedd9de2dbd26ea01449
Lilia <geertszelia@gmail.com>
2016-11-08 12:00:43 +0100

    Incrémentation de l'id à chaque nouveau formulaire

--- server.js
+++ server.js
@@ -9,14 +9,18 @@ app.use(express.static(__dirname + '/public'));
 
 var crm = {
 	updateJson: function(req, res){
-		var post = req.body;
-		var urlCrm = __dirname + '/public/crm.json';
-		fs.readFile(urlCrm, 'utf8', function(err, data){
-			var content = JSON.parse(data);
-			content.customers.push({first_name: post.first_name, last_name: post.last_name, phone: post.phone, email: post.email, description: post.description});
-			var jsonStrig = JSON.stringify(content);
+		var post = req.body; //Données du formulaire
+		var urlCrm = __dirname + '/public/crm.json'; //url du fichier crm.json
+		fs.readFile(urlCrm, 'utf8', function(err, data){ //lecture du fichier json
+			var content = JSON.parse(data); //parse les données du json
+
+			var lenId = content.customers[content.customers.length-1].id; //longueur du tableau selon l'id
+			post.id = lenId +1; //increment l'id en fonction de la longueur du tableau
+
+			content.customers.push({id: post.id, first_name: post.first_name, last_name: post.last_name, phone: post.phone, email: post.email, description: post.description});
+			var jsonStrig = JSON.stringify(content); //met en string les données du json
 			
-			fs.writeFile(urlCrm, jsonStrig, function(err){
+			fs.writeFile(urlCrm, jsonStrig, function(err){ //écrit dans le json les données poussées en string
 				if(err){
 					console.log(err);
 				}
